3a31cfdd5cecd787c0089aed7423d06a16eee0b9
bruno78 <devbruno78@gmail.com>
2018-01-28 20:45:58 -0500

    COMPLETED lesson S04.01

--- S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -16,6 +16,8 @@
 package com.example.android.sunshine;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -117,11 +119,11 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
     @Override
     public void onClick(String weatherForDay) {
         Context context = this;
-        // TODO (1) Create a new Activity called DetailActivity using Android Studio's wizard
-        // TODO (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
-        // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
-        Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-                .show();
+        // COMPLETED (1) Create a new Activity called DetailActivity using Android Studio's wizard
+        // COMPLETED (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
+        // COMPLETED (3) Remove the Toast and launch the DetailActivity using an explicit Intent
+        Intent detailActivityIntent = new Intent(context, DetailActivity.class);
+        startActivity(detailActivityIntent);
     }
 
     /**
