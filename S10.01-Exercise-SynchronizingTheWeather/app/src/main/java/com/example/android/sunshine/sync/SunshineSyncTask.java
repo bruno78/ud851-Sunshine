bf0017eb3b22e50eabe449729c50d593a072ddde
bruno78 <devbruno78@gmail.com>
2018-02-27 14:17:36 -0500

    COMPLETED lesson S10.01

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,39 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.net.URL;
+
+//  COMPLETED (1) Create a class called SunshineSyncTask
+//  COMPLETED (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+//      COMPLETED (3) Within syncWeather, fetch new weather data
+//      COMPLETED (4) If we have valid results, delete the old data and insert the new
+
+public class SunshineSyncTask {
+
+    public static void syncWeather(Context context) {
+
+        try {
+
+            // Gets the URL needed to get the forecast JSON for the weather
+            URL weatherRequestUrl = NetworkUtils.getUrl(context);
+
+            // Uses the URL to retrieve the JSON
+            String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+
+            // Parse the JSON into a list of weather values
+            ContentValues[] weatherValues = OpenWeatherJsonUtils
+                    .getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,3 +1,17 @@
-// TODO (9) Create a class called SunshineSyncUtils
-    //  TODO (10) Create a public static void method called startImmediateSync
-    //  TODO (11) Within that method, start the SunshineSyncIntentService
+package com.example.android.sunshine.sync;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.android.sunshine.sync.SunshineSyncIntentService;
+
+// COMPLETED (9) Create a class called SunshineSyncUtils
+public class SunshineSyncUtils {
+
+    //  COMPLETED (10) Create a public static void method called startImmediateSync
+    //  COMPLETED (11) Within that method, start the SunshineSyncIntentService
+    public static void startImmediateSync(final Context context) {
+        Intent intent = new Intent(context, SunshineSyncIntentService.class);
+        context.startService(intent);
+    }
+}
